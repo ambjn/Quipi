aeeea981934bae3a21c578b7a768a97a0e347c9e
Amber Jain <jainanuamber@gmail.com>
2023-01-26 18:48:02 +0530

    updated

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:quipi/models/note_model.dart';
 
 class NoteCard extends StatelessWidget {
   const NoteCard({
